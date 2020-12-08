5362200e376da92e317da2149a20f8fc853b8f90
jiabinbin <jia_bin1115@163.com>
2020-12-08 23:48:03 +0800

    update StreamTest

--- jdk-test/src/com/jaibb/stream/StreamTest.java
+++ jdk-test/src/com/jaibb/stream/StreamTest.java
@@ -27,22 +27,12 @@ public class StreamTest {
 
     @Before
     public void init(){
-        personList.add(new
-                Person("Tom",8900,"male","New York"));
-        personList.add(new
-                Person("Jack",7000,"male","Washington"));
-        personList.add(new
-
-                Person("Lily",7800,"female","Washington"));
-        personList.add(new
-
-                Person("Anni",8200,"female","New York"));
-        personList.add(new
-
-                Person("Owen",9500,"male","New York"));
-        personList.add(new
-
-                Person("Alisa",7900,"female","New York"));
+        personList.add(new Person("Tom",7000,"male","New York"));
+        personList.add(new Person("Jack",7000,"male","Washington"));
+        personList.add(new Person("Lily",7800,"female","Washington"));
+        personList.add(new Person("Anni",8200,"female","New York"));
+        personList.add(new Person("Owen",9500,"male","New York"));
+        personList.add(new Person("Alisa",7900,"female","New York"));
     }
 
     @Test
@@ -256,13 +246,129 @@ public class StreamTest {
         out.println(stream4.reduce(0, (x, y) -> x + y, (x, y) -> x * y));
     }
 
+    @Test
+    public void toList_toSet_toMap(){
+        List<Integer> list = Arrays.asList(1, 6, 3, 4, 6, 7, 9, 6, 20);
+        List<Integer> listNew = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        Set<Integer> set = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toSet());
 
 
+        Map<?, Person> map = personList.stream().filter(p -> p.getSalary() > 8000)
+                .collect(Collectors.toMap(Person::getName, p -> p));
+        System.out.println("toList:" + listNew);
+        System.out.println("toSet:" + set);
+        System.out.println("toMap:" + map);
+    }
 
+    @Test
+    public void count_averaging(){
+        // 求总数
+        Long count = personList.stream().collect(Collectors.counting());
+        assertThat(6L, Is.is(count));
+        // 求平均工资
+        Double average = personList.stream().collect(Collectors.averagingDouble(Person::getSalary));
+        assertThat(8216.666666666666, Is.is(average));
+        // 求最高工资
+        Optional<Integer> max = personList.stream().map(Person::getSalary).collect(Collectors.maxBy(Integer::compare));
+        assertThat(9500, Is.is(max.get()));
+        // 求工资之和
+        Integer sum = personList.stream().collect(Collectors.summingInt(Person::getSalary));
+        assertThat(49300, Is.is(sum));
+        // 一次性统计所有信息
+        DoubleSummaryStatistics collect = personList.stream().collect(Collectors.summarizingDouble(Person::getSalary));
+        System.out.println(collect);
+    }
 
+    @Test
+    public void partitioningBy_groupingBy(){
+        // 将员工按薪资是否高于8000分组
+        Map<Boolean, List<Person>> part = personList.stream().collect(Collectors.partitioningBy(x -> x.getSalary() > 8000));
+        // 将员工按性别分组
+        Map<String, List<Person>> group = personList.stream().collect(Collectors.groupingBy(Person::getSex));
+        // 将员工先按性别分组，再按地区分组
+        Map<String, Map<String, List<Person>>> group2 = personList.stream()
+                .collect(Collectors.groupingBy(Person::getSex, Collectors.groupingBy(Person::getArea)));
+        System.out.println("员工按薪资是否大于8000分组情况：" + part);
+        System.out.println("员工按性别分组情况：" + group);
+        System.out.println("员工按性别、地区：" + group2);
+    }
 
+    @Test
+    public void joining(){
+
+        String names = personList.stream().map(p -> p.getName()).collect(Collectors.joining(","));
+        System.out.println("所有员工的姓名：" + names);
+        List<String> list = Arrays.asList("A", "B", "C");
+        String string = list.stream().collect(Collectors.joining("-"));
+        System.out.println("拼接后的字符串：" + string);
 
     }
+
+    @Test
+    public void Collectors_reducing(){
+        // 每个员工减去起征点后的薪资之和（这个例子并不严谨，但一时没想到好的例子）
+        Integer sum = personList.stream().collect(Collectors.reducing(0, Person::getSalary, (i, j) -> (i + j - 5000)));
+        System.out.println("员工扣税薪资总和：" + sum);
+
+        // stream的reduce
+        Optional<Integer> sum2 = personList.stream().map(Person::getSalary).reduce(Integer::sum);
+        System.out.println("员工薪资总和：" + sum2.get());
+    }
+
+    @Test
+    public void sorted(){
+        // 按工资增序排序
+        List<String> newList = personList.stream().sorted(Comparator.comparing(Person::getSalary)).map(Person::getName)
+                .collect(Collectors.toList());
+        // 按工资倒序排序
+        List<String> newList2 = personList.stream().sorted(Comparator.comparing(Person::getSalary).reversed())
+                .map(Person::getName).collect(Collectors.toList());
+        // 先按工资再按年龄自然排序（从小到大）
+        List<String> newList3 = personList.stream().sorted(Comparator.comparing(Person::getSalary).reversed())
+                .sorted(Comparator.comparing(Person::getArea))
+                .map(Person::getName).collect(Collectors.toList());
+        // 先按工资再按年龄自定义排序（从大到小）
+//        List<String> newList4 = personList.stream().sorted((p1, p2) -> {
+//            if (p1.getSalary() == p2.getSalary()) {
+//                return p2.getAge() - p1.getAge();
+//            } else {
+//                return p2.getSalary() - p1.getSalary();
+//            }
+//        }).map(Person::getName).collect(Collectors.toList());
+
+        System.out.println("按工资自然排序：" + newList);
+        System.out.println("按工资降序排序：" + newList2);
+        System.out.println("先按工资再按地区：" + newList3);
+//        System.out.println("先按工资再按年龄自定义降序排序：" + newList4);
+    }
+
+    @Test
+    public void distinct_skip_limit(){
+        String[] arr1 = { "a", "b", "c", "d" };
+        String[] arr2 = { "d", "e", "f", "g" };
+
+        Stream<String> stream1 = Stream.of(arr1);
+        Stream<String> stream2 = Stream.of(arr2);
+        // concat:合并两个流 distinct：去重
+        List<String> newList = Stream.concat(stream1, stream2).distinct().collect(Collectors.toList());
+        // limit：限制从流中获得前n个数据
+        List<Integer> collect = Stream.iterate(1, x -> x + 2).limit(10).collect(Collectors.toList());
+        // skip：跳过前n个数据
+        List<Integer> collect2 = Stream.iterate(1, x -> x + 2).skip(2).limit(5).collect(Collectors.toList());
+
+        System.out.println("流合并：" + newList);
+        System.out.println("limit：" + collect);
+        System.out.println("skip：" + collect2);
+    }
+
+}
+
+
+
+
+
+
+
 
 class Person {
     // 姓名
